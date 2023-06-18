c6e9cbced448fa50331fe44311e45793405734e2
Amazing Agu <enyichiaagu@gmail.com>
2023-06-18 16:17:00 +0100

    Simple http server with barebones node.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const http = require('node:http')
+
+const PORT = 3000
+
+const server = http.createServer()
+
+const friends = [
+    {
+        id: 0,
+        name: 'Sir Isaac Newton'
+    },
+    {
+        id: 1,
+        name: 'Albert Einstein'
+    },
+    {
+        id: 2,
+        name: 'Nikola Tesla'
+    }
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/')
+    console.log(items)
+    if (items[1] === 'friends') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        if (items.length === 3) {
+            const friendIndex = Number(items[2])
+            res.end(JSON.stringify(friends[friendIndex]))
+        } else {
+            res.end(JSON.stringify(friends))
+        }
+    } else if (items[1] === 'messages') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<head>')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<ul>')
+        res.write('<li>Hello Isaac!</li>')
+        res.write('<li>What are your thoughts on Astronomy</li>')
+        res.write('</ul>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end()
+    } else {
+        res.statusCode = 404
+        res.end()
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}...`)
+})
